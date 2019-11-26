0a7c411ef149e5c0179b45c741798b83b85ad9d6
bunnyy <joewlozg@gmail.com>
2019-11-26 17:58:06 +0800

    RspecDelete

--- spec/features/mission_spec.rb
+++ spec/features/mission_spec.rb
@@ -28,33 +28,17 @@ RSpec.feature "Missions", type: :feature do
     expect(page).to have_content "更新任務成功"
   end
 
-  # scenario "可以新增一個任務" do
-  #   visit '/missions/new'
-
-  #   fill_in 'mission_name', with: 'cccccc002'
-  #   select '機密', from: 'mission_priority'
-  #   select '已通過', from: 'mission_status'
-  #   click_button '確認送出'
-
-  #   expect(page).to have_text("新增任務成功")
-  #   save_and_open_page
-  # end
-
-  # scenario "任務清單有照時間排序" do
-  #   visit '/missions/new'
-
-  #   fill_in 'mission_name', with: 'cccccc002'
-  #   select '機密', from: 'mission_priority'
-  #   select '已通過', from: 'mission_status'
-  #   click_button '確認送出'
-
-  #   visit '/missions/new'
-
-  #   fill_in 'mission_name', with: 'aaaaaa002'
-  #   select '一般', from: 'mission_priority'
-  #   select '已取消', from: 'mission_status'
-  #   click_button '確認送出'
-
-  #   expect(Mission.find(2).name).to eq "aaaaaa002"
-  # end
+  scenario "刪除任務" do
+    begin
+      # put line of code which triggers alert on page
+    rescue Selenium::WebDriver::Error::UnhandledAlertError
+      @browser.alert.ok 
+      FactoryBot.create(:mission, name: "cccccc002")
+      visit root_path
+      click_on "刪除"
+      click_on "確認"
+      expect(page).not_to have_content "cccccc002"
+      expect(page).to have_content "任務刪除成功"
+    end
+  end
 end
