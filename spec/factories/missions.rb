70ab9e76a06daa57dc4dc8a9ff4924a16e8e63fb
bunnyy <joewlozg@gmail.com>
2019-11-26 17:31:26 +0800

    RspecUpdate

--- spec/factories/missions.rb
+++ spec/factories/missions.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :mission do
-    user { User.first }
     name { Faker::Games::LeagueOfLegends.champion }
   end
 end
--- spec/features/mission_spec.rb
+++ spec/features/mission_spec.rb
@@ -8,13 +8,24 @@ RSpec.feature "Missions", type: :feature do
   scenario "新增任務" do
     visit root_path
     click_on "新增任務"
-    fill_in 'mission_name', with: 'cccccc002'
-    select '機密', from: 'mission_priority'
-    select '已通過', from: 'mission_status'
-    click_on "送出"
+    fill_in 'mission_name', with: "cccccc002"
+    select '機密', from: "mission_priority"
+    select '已通過', from: "mission_status"
+    click_on "確認送出"
 
     expect(page).to have_content "cccccc002"
     expect(page).to have_content "新增任務成功"
+  end
+
+  scenario "編輯任務" do
+    FactoryBot.create(:mission, name: "cccccc002")
+    visit root_path
+    click_on "編輯"
+    fill_in 'mission_name', with: "aaaaaa002"
+    click_on "確認送出"
+
+    expect(page).to have_content "aaaaaa002"
+    expect(page).to have_content "更新任務成功"
   end
 
   # scenario "可以新增一個任務" do
