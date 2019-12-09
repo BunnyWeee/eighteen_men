907eb9ca42d0bc97afe5b42812790b5be050749b
bunnyy <joewlozg@gmail.com>
2019-12-10 01:17:15 +0800

    fixed_model_message_i18n

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -4,7 +4,7 @@ class Mission < ApplicationRecord
   enum priority:{ important: 0, normal: 1 }
   enum status:{ pending: 0, approved: 1, rejected: 2, canceled: 3 }
 
-  validates :name, format: { with: /\A[A-Z0-9]+\z/, message: "請注意英文只能使用大寫！" }, presence: true
+  validates :name, format: { with: /\A[A-Z0-9]+\z/, message: :uppercaseonly }, presence: true
   validates :description, presence: true
   # validates_datetime :complete_time, :after => :start_time 
   scope :order_by_complete_time, -> { order(complete_time: :asc) }
