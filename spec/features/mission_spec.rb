1bbc79317380aa6d3ff73d2638c523563000ca92
bunnyy <joewlozg@gmail.com>
2019-12-14 23:51:30 +0800

    rm#

--- spec/features/mission_spec.rb
+++ spec/features/mission_spec.rb
@@ -30,42 +30,42 @@ RSpec.feature "Missions", type: :feature do
     
   end
 
-  # scenario "新增任務" do
-  #   visit root_path
-  #   click_on "新增任務"
-  #   fill_in 'mission_name', with: "NEWMISSION"
-  #   select '機密', from: "mission_priority"
-  #   select '已通過', from: "mission_status"
-  #   fill_in 'mission_description', with: "description_test"
-  #   click_on "確認送出"
+  scenario "新增任務" do
+    visit root_path
+    click_on "新增任務"
+    fill_in 'mission_name', with: "NEWMISSION"
+    select '機密', from: "mission_priority"
+    select '已通過', from: "mission_status"
+    fill_in 'mission_description', with: "description_test"
+    click_on "確認送出"
 
-  #   expect(page).to have_content "NEWMISSION"
-  #   expect(page).to have_content "新增任務成功"
-  # end
+    expect(page).to have_content "NEWMISSION"
+    expect(page).to have_content "新增任務成功"
+  end
 
-  # scenario "編輯任務" do
-  #   FactoryBot.create(:mission, name: "ORIGINMISSION")
-  #   visit root_path
-  #   click_on "編輯"
-  #   fill_in 'mission_name', with: "EDITMISSION"
-  #   click_on "確認送出"
+  scenario "編輯任務" do
+    FactoryBot.create(:mission, name: "ORIGINMISSION")
+    visit root_path
+    click_on "編輯"
+    fill_in 'mission_name', with: "EDITMISSION"
+    click_on "確認送出"
 
-  #   expect(page).to have_content "EDITMISSION"
-  #   expect(page).to have_content "更新任務成功"
-  # end
+    expect(page).to have_content "EDITMISSION"
+    expect(page).to have_content "更新任務成功"
+  end
 
-  # scenario "刪除任務" do
-  #   begin
-  #     # put line of code which triggers alert on page
-  #   rescue Selenium::WebDriver::Error::UnhandledAlertError
-  #     @browser.alert.ok 
-  #     FactoryBot.create(:mission)
-  #     visit root_path
-  #     click_on "刪除"
-  #     click_on "確認"
-  #     expect(page).not_to have_content "NEWMISSION"
-  #     expect(page).to have_content "任務刪除成功"
-  #   end
-  # end
+  scenario "刪除任務" do
+    begin
+      # put line of code which triggers alert on page
+    rescue Selenium::WebDriver::Error::UnhandledAlertError
+      @browser.alert.ok 
+      FactoryBot.create(:mission)
+      visit root_path
+      click_on "刪除"
+      click_on "確認"
+      expect(page).not_to have_content "NEWMISSION"
+      expect(page).to have_content "任務刪除成功"
+    end
+  end
 
 end
